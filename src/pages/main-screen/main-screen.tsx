d033299019b6ec3e376698a018e7706b4f48fb9f
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-03-04 14:29:13 +0700

    Вынесет остальные компоненты главного экрана

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -1,5 +1,6 @@
 import Locations from '../../components/locations/locations';
-import PlaceCard from '../../components/place-card/place-card';
+import Map from '../../components/map/map';
+import Places from '../../components/places/places';
 import { CITIES } from '../../data';
 import { Place } from '../../types/place.type';
 
@@ -46,32 +47,9 @@ function MainScreen({places}: MainScreenProps): JSX.Element {
         </div>
         <div className="cities">
           <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                                    Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
-              <div className="cities__places-list places__list tabs__content">
-                {places.map((place) => (
-                  <PlaceCard key={`${place.id}`} place={place} />
-                ))}
-              </div>
-            </section>
+            <Places places={places} />
             <div className="cities__right-section">
-              <section className="cities__map map"></section>
+              <Map />
             </div>
           </div>
         </div>
