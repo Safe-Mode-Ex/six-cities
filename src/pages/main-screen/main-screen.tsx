5c6b306f4134a1b80796166b626b4cd31ce240fa
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-03-04 14:05:18 +0700

    Вынесет комопнент Locations

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -1,4 +1,6 @@
+import Locations from '../../components/locations/locations';
 import PlaceCard from '../../components/place-card/place-card';
+import { CITIES } from '../../data';
 import { Place } from '../../types/place.type';
 
 type MainScreenProps = {
@@ -40,40 +42,7 @@ function MainScreen({places}: MainScreenProps): JSX.Element {
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active">
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
-          </section>
+          <Locations cities={CITIES} activeCity={CITIES[3]} />
         </div>
         <div className="cities">
           <div className="cities__places-container container">
