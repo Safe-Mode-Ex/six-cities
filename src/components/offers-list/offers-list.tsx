fec772f9139a19bd55541fb48bddf45c777384ce
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-03-11 00:57:41 +0700

    Подключит classNames для списка карточек

--- src/components/offers-list/offers-list.tsx
+++ src/components/offers-list/offers-list.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import { Offer } from '../../types/offer.type';
 import OfferCard from '../offer-card/offer-card';
 
@@ -9,24 +10,18 @@ type OffersListProps = {
 };
 
 function OffersList({ offers, setActiveOfferId, isFavoritesScreen = false, isOfferScreen = false }: OffersListProps): JSX.Element {
-  const getClassName = () => {
-    if (isFavoritesScreen) {
-      return 'favorites__places';
-    }
-
-    if (isOfferScreen) {
-      return 'near-places__list places__list';
-    }
-
-    return 'cities__places-list places__list tabs__content';
-  };
-
   const handleOfferHover = (offerId: number | null) => {
     setActiveOfferId?.(offerId);
   };
 
   return (
-    <div className={getClassName()}>
+    <div className={cn({
+      'favorites__places': isFavoritesScreen,
+      'places__list': !isFavoritesScreen,
+      'near-places__list': isOfferScreen,
+      'cities__places-list tabs__content': !isFavoritesScreen && !isOfferScreen,
+    })}
+    >
       {offers.map((offer) => (
         <OfferCard
           key={`${offer.id}`}
