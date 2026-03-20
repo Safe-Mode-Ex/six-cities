56dfebdd09b5747d25ee8c5c766221c3a5b17e25
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-03-20 11:47:39 +0300

    Переименует действия

--- src/store/action.ts
+++ src/store/action.ts
@@ -6,10 +6,10 @@ import { AppRoute } from '../types/app-route';
 
 export const REDIRECT_TO_ROUTE_ACTION_NAME = 'app/redirectToRoute';
 
-export const selectCity = createAction<string>('SELECT_CITY');
+export const selectCity = createAction<string>('app/selectCity');
 export const loadOffers = createAction<Offer[]>('data/loadOffers');
 export const setOffersDataLoadingStatus = createAction<boolean>('data/setOffersDataLoadingStatus');
-export const setSortType = createAction<SortType>('SET_SORT_TYPE');
-export const setActiveOfferId = createAction<number | null>('SET_ACTIVE_OFFER_ID');
+export const setSortType = createAction<SortType>('app/setSortType');
+export const setActiveOfferId = createAction<number | null>('app/setActiveOfferId');
 export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
 export const redirectToRoute = createAction<AppRoute>(REDIRECT_TO_ROUTE_ACTION_NAME);
