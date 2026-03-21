4607e8baf92aaabdcfc1bbf5bf743c1e3aa00474
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-03-21 12:34:46 +0300

    Исправит обработку ошибок

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,26 @@
-import axios, { AxiosError, AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import { getToken } from './token';
 import { toast } from 'react-toastify';
 import { StatusCodes } from 'http-status-codes';
 
 type DetailMessageType = {
-  type: string;
+  errorType: string;
   message: string;
+  details: {
+    messages: string[];
+  }[];
 }
 
 const BASE_URL = 'https://16.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: false,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 export const createApi = (): AxiosInstance => {
   const api = axios.create({
@@ -30,9 +41,9 @@ export const createApi = (): AxiosInstance => {
   api.interceptors.response.use(
     (response) => response,
     (error: AxiosError<DetailMessageType>) => {
-      if (error.response && (error.response.status as StatusCodes) !== StatusCodes.UNAUTHORIZED) {
-        const detailMessage = error.response.data;
-        toast.error(detailMessage.message);
+      if (error.response && shouldDisplayError(error.response)) {
+        const detailMessages = error.response.data.details.flatMap(({ messages }) => messages);
+        detailMessages.forEach((message) => toast.error(message));
       }
 
       throw error;
