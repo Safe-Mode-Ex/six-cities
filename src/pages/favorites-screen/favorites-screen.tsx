d83f03c41f6e18c13ff157c1e01fd234aa65b355
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-03-26 17:28:04 +0300

    Отрисует компонент пустого избранного

--- src/pages/favorites-screen/favorites-screen.tsx
+++ src/pages/favorites-screen/favorites-screen.tsx
@@ -4,10 +4,14 @@ import { useAppDispatch, useAppSelector } from '../../hooks/use-app-selector';
 import { fetchFavoriteOffersAction } from '../../store/api-actions';
 import { getFavorite } from '../../store/favorite/selector';
 import { useEffect } from 'react';
+import NoFavorites from '../../components/no-favorites/no-favorites';
+import cn from 'classnames';
 
 function FavoritesScreen(): JSX.Element {
   const dispatch = useAppDispatch();
   const favorite = useAppSelector(getFavorite);
+  const favoriteEntries = Object.entries(favorite);
+  const hasFavorites = favoriteEntries.length;
 
   useEffect(() => {
     let isMounted = true;
@@ -22,29 +26,39 @@ function FavoritesScreen(): JSX.Element {
   }, [dispatch]);
 
   return (
-    <div className="page">
+    <div className={cn(
+      'page',
+      { 'page--favorites-empty': !hasFavorites }
+    )}
+    >
       <Header />
 
-      <main className="page__main page__main--favorites">
+      <main className={cn(
+        'page__main page__main--favorites',
+        { 'page__main--favorites-empty': !hasFavorites }
+      )}
+      >
         <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {Object.entries(favorite).map(([city, offers]) => (
-                <li className="favorites__locations-items" key={city}>
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <a className="locations__item-link" href="#">
-                        <span>{city}</span>
-                      </a>
+          {hasFavorites ? (
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                {favoriteEntries.map(([city, offers]) => (
+                  <li className="favorites__locations-items" key={city}>
+                    <div className="favorites__locations locations locations--current">
+                      <div className="locations__item">
+                        <a className="locations__item-link" href="#">
+                          <span>{city}</span>
+                        </a>
+                      </div>
                     </div>
-                  </div>
 
-                  <OffersList offers={offers} isFavoritesScreen />
-                </li>
-              ))}
-            </ul>
-          </section>
+                    <OffersList offers={offers} isFavoritesScreen />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          ) : <NoFavorites />}
         </div>
       </main>
       <footer className="footer container">
