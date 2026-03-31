b2a83ca1dfa03a545d23651eeb5c92ad43681a9d
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-03-31 22:18:00 +0300

    Стилизует экран загрузки

--- src/pages/loading-screen/loading-screen.tsx
+++ src/pages/loading-screen/loading-screen.tsx
@@ -1,5 +1,14 @@
+import styles from './loading-screen.module.css';
+
 function LoadingScreen(): JSX.Element {
-  return <p>Loading ...</p>;
+  return (
+    <div className={styles.loading}>
+      <p className={styles.loading__text}>
+        <b>Loading ...</b>
+      </p>
+      <p className={styles.loading__desc}>Take a deep breath</p>
+    </div>
+  );
 }
 
 export default LoadingScreen;
